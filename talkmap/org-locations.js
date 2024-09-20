94e3531587ba56e189b2fc31e3f0353bde4cccd1
allie-sinclair <50889052+allie-sinclair@users.noreply.github.com>
2024-09-20 14:32:23 -0400

    Add files via upload

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,67 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Durham, NC, USA",
+    35.996653,
+    -78.9018053
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Cape Cod, MA, USA",
+    41.7987588,
+    -69.99606545468316
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Huntington Beach, CA, USA",
+    33.6783336,
+    -118.000016
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "College Park, MD, USA",
+    38.980666,
+    -76.9369189
+  ],
+  [
+    "New York City, NY, USA",
+    40.7127281,
+    -74.0060152
+  ],
+  [
+    "San Francisco, CA, USA",
+    37.7792588,
+    -122.4193286
+  ],
+  [
+    "Toronto, ON, Canada",
+    43.6534817,
+    -79.3839347
+  ],
+  [
+    "Atlanta, GA, USA",
+    33.7489924,
+    -84.3902644
+  ],
+  [
+    "Philadelphia, PA, USA",
+    39.9527237,
+    -75.1635262
+  ],
+  [
+    "Nagoya, Japan",
+    35.1851045,
+    136.8998438
+  ],
+  [
+    "Camden, NJ, USA",
+    39.9448402,
+    -75.1198911
+  ],
+  [
+    "Hamilton, ON, Canada",
+    43.2560802,
+    -79.8728583
+  ],
+  [
+    "Princeton, NJ, USA",
+    40.3496953,
+    -74.6597376
   ]
 ];
